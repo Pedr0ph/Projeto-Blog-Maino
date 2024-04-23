b7d19894e74dec41efd9adad17973270b7314ecc
Pedro Figueiredo <pedrophcrf@icloud.com>
2024-04-22 23:04:28 -0300

    Adicionando autor nos posts

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   has_many :comments
 
+  belongs_to :author, class_name: "User", foreign_key: :user_id
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_003153) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_011208) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -38,5 +40,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_003153) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
